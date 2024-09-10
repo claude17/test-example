5ca8ef82f438036a2b9cb83955846f17de326623
claude17 <claude.mta@gmail.com>
2024-09-10 06:58:17 +0600

    Add files via upload

---
+++ example/Controllers/EmployeeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCRUDEF.Context;
+
+namespace WebAppCRUDEF.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        AspBDbEntitiesContext _dbContext;
+
+        public EmployeeController()
+        {
+            _dbContext = new AspBDbEntitiesContext();
+        }
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            if (Session["username"] == null)
+                return RedirectToAction("Login", "User");
+
+            var empList = _dbContext.Employees.ToList();
+
+            if (empList == null)
+                return HttpNotFound();
+
+            return View(empList);
+        }
+
+        [HttpGet]
+        public ActionResult AddEmployee(Employee e)
+        {
+            if (Session["username"] == null)
+                return RedirectToAction("Login", "User");
+
+            if (Session["type"].ToString().Equals("member"))
+                return RedirectToAction("Index", "Employee");
+
+            if (e.Id > 0)
+                return View(e);
+            else
+            {
+                ModelState.Clear();
+                ViewBag.NoData = 0;
+                return View();
+            }
+                
+        }
+
+        [HttpPost]
+        public ActionResult CreateEmployee(Employee emp)
+        {
+            ModelState.Remove("Id");
+            if(ModelState.IsValid)
+            {
+                if(emp.Id <=0)
+                {
+                    _dbContext.Employees.Add(emp);
+                    _dbContext.SaveChanges();
+                    TempData["MsgAdd"] = "Employee information added successfully";
+                }
+                else
+                {
+                    _dbContext.Entry(emp).State = EntityState.Modified;
+                    _dbContext.SaveChanges();
+                    TempData["MsgMod"] = "Employee information modified successfully";
+                }
+
+                return RedirectToAction("Index");
+                
+            }
+
+            return View("AddEmployee");
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var data = _dbContext.Employees.Where(x => x.Id == id).First();
+            _dbContext.Employees.Remove(data);
+            _dbContext.SaveChanges();
+            TempData["MsgRem"] = "Employee information removed successfully";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ example/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCRUDEF.Context;
+
+namespace WebAppCRUDEF.Controllers
+{
+    public class UserController : Controller
+    {
+        AspBDbEntitiesContext _dbContext;
+
+        public UserController()
+        {
+            this._dbContext = new AspBDbEntitiesContext();
+        }
+
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            ModelState.Remove("RePassword");
+            if(ModelState.IsValid)
+            {
+                var data = _dbContext.Users.Where(x => x.Username == u.Username && x.Password == u.Password).FirstOrDefault();
+                if(data != null)
+                {
+                    Session["id"] = data.Id;
+                    Session["username"] = data.Username;
+                    Session["type"] = data.Type;
+                    return RedirectToAction("Index", "Employee");
+                }
+                ViewBag.Invalid = "Invalid User";
+                ModelState.Clear();
+                return View();
+            }
+
+            ModelState.Clear();
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            TempData["Logout"] = "Logged out from the system";
+
+            return View("Login");
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User u)
+        {
+            if(_dbContext.Users.Any(x => x.Username == u.Username))
+            {
+                ModelState.Clear();
+                return View();
+            }
+
+            _dbContext.Users.Add(u);
+            _dbContext.SaveChanges();
+
+            return View("Login");
+        }
+
+        public ViewResult ClearLogin()
+        {
+            ModelState.Clear();
+            return View("Login");
+        }
+
+        public ViewResult ClearSignUp()
+        {
+            ModelState.Clear();
+            return View("SignUp");
+        }
+    }
+}
